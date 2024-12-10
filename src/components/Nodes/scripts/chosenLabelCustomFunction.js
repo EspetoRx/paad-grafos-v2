f1eca1052339b95050175e7e5b23d0ed38930742
Lucas Carvalho <lcdo2395@gmail.com>
2024-12-10 00:09:28 -0300

    Hotfix applied to Nodes Selected.

---
+++ src/components/Nodes/scripts/chosenLabelCustomFunction.js
@@ -0,0 +1,10 @@
+((values, id, selected, hovering) => {
+    //values.property
+    values.color = "#000000";
+    values.size = 18;
+    //values.face
+    //values.mod -- bold, italic, etc
+    //values.vadjust
+    //values.strokeWidth
+    //values.strokeColor
+})
